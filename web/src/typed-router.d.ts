7039bc49bb79854c1e6e240e4513719b33c08a78
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2026-07-28 13:22:55 +0100

    fix(web): Add the missing /parts SPA rewrite so refreshes stop 404ing (#472)

--- web/src/typed-router.d.ts
+++ web/src/typed-router.d.ts
@@ -38,6 +38,13 @@ declare module 'vue-router/auto-routes' {
       Record<never, never>,
       | never
     >,
+    '/[...path]': RouteRecordInfo<
+      '/[...path]',
+      '/:path(.*)',
+      { path: ParamValue<true> },
+      { path: ParamValue<false> },
+      | never
+    >,
     '/changelog': RouteRecordInfo<
       '/changelog',
       '/changelog',
@@ -100,6 +107,14 @@ declare module 'vue-router/auto-routes' {
         | never
       pathParamNames:
         | never
+    }
+    'src/pages/[...path].vue': {
+      routes:
+        | '/[...path]'
+      views:
+        | never
+      pathParamNames:
+        | 'path'
     }
     'src/pages/changelog.vue': {
       routes:
